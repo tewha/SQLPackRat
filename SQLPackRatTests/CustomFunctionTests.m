02608904e61ad4ddc45b8944117a2bddd7683c22
Steven Fisher <tewha@me.com>
2015-06-12 18:55:40 -0700

    Remove tests from template.

--- SQLPackRatTests/CustomFunctionTests.m
+++ SQLPackRatTests/CustomFunctionTests.m
@@ -46,16 +46,6 @@
 }
 
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testAggregateBlock {
     NSError *e;
     SQLPackRatDatabase *database = [self open:&e];
@@ -81,16 +71,5 @@
     XCTAssertEqual(sum, 6, @"Sum wrong.");
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
