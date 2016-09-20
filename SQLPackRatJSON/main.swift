65ab1dcc18be64ed9a572c05e9cbda7d45cf0c01
Steven Fisher <tewha@me.com>
2016-09-20 13:09:37 -0700

    Update for Xcode 8/Swift 3.

--- SQLPackRatJSON/main.swift
+++ SQLPackRatJSON/main.swift
@@ -9,20 +9,20 @@
 import Foundation
 
 enum Error:Int32 {
-    case None = 0
-    case DatabaseMissing
-    case DatabaseOpen
-    case BindRead
-    case BindDeserialize
-    case QueryMissing
-    case QueryRead
-    case QueryExec
-    case InputRead
-    case InputDeserialize
-    case InputFormat
-    case KeyMissing
-    case OutputSerialize
-    case OutputWrite
+    case none = 0
+    case databaseMissing
+    case databaseOpen
+    case bindRead
+    case bindDeserialize
+    case queryMissing
+    case queryRead
+    case queryExec
+    case inputRead
+    case inputDeserialize
+    case inputFormat
+    case keyMissing
+    case outputSerialize
+    case outputWrite
 }
 
 func + <K,V>(left: Dictionary<K,V>, right: Dictionary<K,V>)
@@ -38,40 +38,40 @@ func + <K,V>(left: Dictionary<K,V>, right: Dictionary<K,V>)
     return map
 }
 
-@noreturn func Fail(code:Error, message:String) {
-    let data = message.stringByAppendingString("\n").dataUsingEncoding(NSUTF8StringEncoding)!
-    NSFileHandle.fileHandleWithStandardError().writeData(data)
+func Fail(_ code:Error, message:String) -> Never  {
+    let data = (message + "\n").data(using: String.Encoding.utf8)!
+    FileHandle.standardError.write(data)
     exit(code.rawValue)
 }
 
-let parameters = NSUserDefaults.standardUserDefaults().dictionaryRepresentation()
+let parameters = UserDefaults.standard.dictionaryRepresentation()
 
 guard let databasePath = (parameters["database"] as? String) else {
-    Fail(Error.DatabaseMissing, message: "Error opening database: no path")
+    Fail(Error.databaseMissing, message: "Error opening database: no path")
 }
 
 let database:SQLPRDatabase;
 do {
     database = try SQLPRDatabase(path: databasePath, flags: SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE, vfs: nil);
 } catch var error as NSError {
-    Fail(Error.DatabaseOpen, message: String(format:"Error opening database: %@: %@", databasePath, error.localizedDescription))
+    Fail(Error.databaseOpen, message: String(format:"Error opening database: %@: %@", databasePath, error.localizedDescription))
 }
 
 // Build bindings
 var bindings = [String:AnyObject]()
 let bindingsPath = parameters["bindings"] as? String
 if let path = bindingsPath {
-    let data: NSData
+    let data: Data
     do {
-        data = try NSData(contentsOfFile: path, options: [])
+        data = try Data(contentsOf: URL(fileURLWithPath: path), options: [])
     } catch var error as NSError {
-        Fail(Error.BindRead, message: String(format:"Error reading bindings %@: %@", path, error.localizedDescription))
+        Fail(Error.bindRead, message: String(format:"Error reading bindings %@: %@", path, error.localizedDescription))
     }
     let bindingsFromFile:Dictionary<String, AnyObject>
     do {
-        bindingsFromFile = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as! Dictionary<String, AnyObject>
+        bindingsFromFile = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, AnyObject>
     } catch var error as NSError {
-        Fail(Error.BindDeserialize, message: String(format:"Error deserializing bindings %@: %@", path, error.localizedDescription))
+        Fail(Error.bindDeserialize, message: String(format:"Error deserializing bindings %@: %@", path, error.localizedDescription))
     }
     bindings = bindings + bindingsFromFile
 }
@@ -82,49 +82,49 @@ if let query = parameters["query"] as? String {
     SQL = query;
 } else {
     guard let queryPath = parameters["querypath"] as? String else {
-        Fail(Error.QueryMissing, message: "Need -query or -querypath")
+        Fail(Error.queryMissing, message: "Need -query or -querypath")
     }
     do {
-        SQL = try String(contentsOfFile:(queryPath as NSString).stringByExpandingTildeInPath, encoding:NSUTF8StringEncoding)
+        SQL = try String(contentsOfFile:(queryPath as NSString).expandingTildeInPath, encoding:String.Encoding.utf8)
     } catch var error as NSError {
-        Fail(Error.QueryRead, message: String(format:"Error reading -querypath %@: %@", queryPath, error.localizedDescription))
+        Fail(Error.queryRead, message: String(format:"Error reading -querypath %@: %@", queryPath, error.localizedDescription))
     }
 }
 
 // Run the SQL, capturing the resulting records.
 let records:Array<AnyObject>
 do {
-    records = try database.recordsFromSQL(SQL, bindingKeyValues: bindings);
+    records = try database.records(fromSQL: SQL, bindingKeyValues: bindings) as Array<AnyObject>;
 } catch var error as NSError {
-    Fail(Error.QueryExec, message: String(format:"Error running query: %@", error.localizedDescription))
+    Fail(Error.queryExec, message: String(format:"Error running query: %@", error.localizedDescription))
 }
 
 // What we're writing depends on what we're passed.
 // If we're passed an -input, it's a dictionary with the records inserted as -key
 // If we're passed only a -key, it's a dictionary with key:records
 // If we're not passed an input or a key, it's an array.
-let object:AnyObject
+let object:Any
 if let path = (parameters["input"] as? String) ?? (parameters["in"] as? String) {
-    let dataOptional: NSData?
+    let dataOptional: Data?
     do {
-        dataOptional = try NSData(contentsOfFile: (path as NSString).stringByExpandingTildeInPath, options: [])
+        dataOptional = try Data(contentsOf: URL(fileURLWithPath: (path as NSString).expandingTildeInPath), options: [])
     } catch var error as NSError {
-        Fail(Error.InputRead, message: String(format:"Error reading -input %@: %@", path, error.localizedDescription))
+        Fail(Error.inputRead, message: String(format:"Error reading -input %@: %@", path, error.localizedDescription))
     }
     let data = dataOptional!
     
     do {
-        object = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers)
+        object = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
     } catch var error as NSError {
-        Fail(Error.InputDeserialize, message: String(format:"Error deserializing %@: %@", path, error.localizedDescription))
+        Fail(Error.inputDeserialize, message: String(format:"Error deserializing %@: %@", path, error.localizedDescription))
     }
     
     guard let key = parameters["key"] as? String else {
-        Fail(Error.KeyMissing, message: String(format:"-key required if -input specified"))
+        Fail(Error.keyMissing, message: String(format:"-key required if -input specified"))
     }
     
     guard let dictionary = object as? NSMutableDictionary else {
-        Fail(Error.InputFormat, message: String(format:"-input %@ was not a dictionary", path))
+        Fail(Error.inputFormat, message: String(format:"-input %@ was not a dictionary", path))
     }
     dictionary[key] = records
     
@@ -132,20 +132,20 @@ if let path = (parameters["input"] as? String) ?? (parameters["in"] as? String) 
     if let key = parameters["key"] as? String {
         object = [key:records]
     } else {
-        object = records
+        object = records as AnyObject
     }
 }
 
 // Serialize the object to JSON.
 let JSON:String;
 do {
-    let serialized = try NSJSONSerialization.dataWithJSONObject(object, options: NSJSONWritingOptions.PrettyPrinted)
-    guard let string = String(data:serialized, encoding:NSUTF8StringEncoding) else {
-        Fail(Error.OutputWrite, message: String(format:"Error converting output to string"))
+    let serialized = try JSONSerialization.data(withJSONObject: object, options: JSONSerialization.WritingOptions.prettyPrinted)
+    guard let string = String(data:serialized, encoding:String.Encoding.utf8) else {
+        Fail(Error.outputWrite, message: String(format:"Error converting output to string"))
     }
     JSON = string;
 } catch var error as NSError {
-    Fail(Error.OutputSerialize, message: String(format:"Error serializing output: %@", error.localizedDescription))
+    Fail(Error.outputSerialize, message: String(format:"Error serializing output: %@", error.localizedDescription))
 }
 
 
@@ -154,9 +154,9 @@ let outputPath = (parameters["output"] as? String) ?? (parameters["out"] as? Str
 if let path = outputPath {
     // …to a file
     do {
-        try JSON.writeToFile((path as NSString).stringByExpandingTildeInPath, atomically:true, encoding: NSUTF8StringEncoding)
+        try JSON.write(toFile: (path as NSString).expandingTildeInPath, atomically:true, encoding: String.Encoding.utf8)
     } catch var error as NSError {
-        Fail(Error.OutputWrite, message: String(format:"Error writing output %@: %@", path, error.localizedDescription))
+        Fail(Error.outputWrite, message: String(format:"Error writing output %@: %@", path, error.localizedDescription))
     }
 } else {
     // …or to stdout
